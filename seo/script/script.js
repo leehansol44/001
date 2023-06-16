779d8261bd946c9fda8d2a1a2a0bf1d904a832ae
hansol lee <leehansol443@gmail.com>
2023-06-16 10:31:55 -0700

    first commit

---
+++ seo/script/script.js
@@ -0,0 +1,5 @@
+$(function(){
+    const toggleBtn = document.querySelector('.navbar_togglebtn');
+    const menu = document.querySelector('.navbar_menu');
+    const icons = document.querySelector('.navbaricons');
+})
